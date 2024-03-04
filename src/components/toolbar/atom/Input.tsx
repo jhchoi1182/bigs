4a9f445e486e77ba8b0adb4b98100a5b549d32c2
jhchoi1182 <jhchoi1182@gmail.com>
2024-03-05 04:38:12 +0900

    feat: input 컴포넌트 추가

---
+++ src/components/toolbar/atom/Input.tsx
@@ -0,0 +1,36 @@
+import Icon from "@/components/base/Icon";
+import SearchIcon from "@/components/icons/SearchIcon";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  isIcon?: boolean;
+}
+
+export function useInput({ isIcon, ...props }: InputProps) {
+  const [value, setValue] = useState("");
+  const input = (
+    <InputRelativeDiv>
+      <Input onChange={(e) => setValue(e.target.value)} {...props} />
+      {isIcon && <SearchIcon />}
+    </InputRelativeDiv>
+  );
+  return [value, input];
+}
+
+const Input = styled.input`
+  padding: 0.5rem 1rem 0.5rem 1rem;
+  border: 1px solid;
+  border-radius: 10px;
+`;
+export default Input;
+
+const InputRelativeDiv = styled.div`
+  position: relative;
+  ${Icon} {
+    position: absolute;
+    top: 50%;
+    right: 0.5rem;
+    transform: translateY(-50%);
+  }
+`;
--- src/components/toolbar/organisms/SearchBar.tsx
+++ src/components/toolbar/organisms/SearchBar.tsx
@@ -1,24 +1,37 @@
 import React, { useState } from "react";
 import FilterSelect from "../molecules/FilterSelect";
 import styled from "styled-components";
+import Input, { useInput } from "../atom/Input";
 
 export default function SearchBar() {
   const [selectedFilter, setSelectedFilter] = useState("제목");
+  const [searchKeyword, input] = useInput({ isIcon: true, id: "search-input", placeholder: "검색어를 입력해주세요." });
+
   const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedFilter(event.target.value);
   };
 
-  console.log(selectedFilter);
-
   return (
     <SearchBarBox>
-      <form>
+      <FormBox>
         <FilterSelect selectedFilter={selectedFilter} handleSelectChange={handleSelectChange} />
-      </form>
+        {input}
+      </FormBox>
     </SearchBarBox>
   );
 }
 
 const SearchBarBox = styled.div`
   align-self: flex-end;
+`;
+
+const FormBox = styled.form`
+  display: flex;
+  align-items: center;
+  gap: 5%;
+  ${Input} {
+    width: 40rem;
+    height: 3rem;
+    background-color: var(--silver);
+  }
 `;
