924a50f384895de9045d4452962036015d9c6dc8
jhchoi1182 <jhchoi1182@gmail.com>
2024-03-05 04:36:28 +0900

    feat: 아이콘 스타일드 컴포넌트 추가

---
+++ src/components/base/Icon.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Icon = styled.button`
+  width: 3.5rem;
+  height: 3.5rem;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+export default Icon;
---
+++ src/components/icons/SearchIcon.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Icon from "../base/Icon";
+
+export default function SearchIcon() {
+  return (
+    <Icon>
+      <svg viewBox="0 0 78 78" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M55.5 55.5L72 72" stroke="black" stroke-width="10.9722" stroke-linecap="round" stroke-linejoin="round" />
+        <path
+          d="M6 34.2857C6 49.9074 18.6639 62.5714 34.2857 62.5714C42.11 62.5714 49.1928 59.3945 54.3134 54.2601C59.4166 49.1437 62.5714 42.0831 62.5714 34.2857C62.5714 18.6639 49.9074 6 34.2857 6C18.6639 6 6 18.6639 6 34.2857Z"
+          stroke="black"
+          stroke-width="10.9722"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </Icon>
+  );
+}
