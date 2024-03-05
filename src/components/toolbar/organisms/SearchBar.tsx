70f3c6d0dfc9caaeb002b5466e13cfb033135b56
jhchoi1182 <jhchoi1182@gmail.com>
2024-03-05 22:00:32 +0900

    feat: 검색창 오른쪽 패딩 추가

--- src/components/toolbar/organisms/SearchBar.tsx
+++ src/components/toolbar/organisms/SearchBar.tsx
@@ -6,7 +6,7 @@ import Button from "@/components/base/Button";
 
 export default function SearchBar() {
   const [selectedFilter, setSelectedFilter] = useState("제목");
-  const { value, setValue, input } = useInput({ isIcon: true, id: "search-input", placeholder: "검색어를 입력해주세요." });
+  const { value, input } = useInput({ isIcon: true, id: "search-input", placeholder: "검색어를 입력해주세요." });
 
   const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedFilter(event.target.value);
@@ -15,7 +15,6 @@ export default function SearchBar() {
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
     console.log(selectedFilter, value);
-    setValue("");
   };
 
   return (
@@ -40,6 +39,7 @@ const FormBox = styled.form`
   ${Input} {
     width: 40rem;
     height: 3rem;
+    padding-right: 4rem;
     background-color: var(--silver);
   }
 `;
