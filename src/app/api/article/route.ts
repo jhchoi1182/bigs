d005a8891efb72d33bd8f60bfc8a333c35ec9ca9
jhchoi1182 <jhchoi1182@gmail.com>
2024-03-06 20:15:09 +0900

    fix: 상세 페이지 조회 시 undefined 뜨는 케이스 해결

--- src/app/api/article/route.ts
+++ src/app/api/article/route.ts
@@ -4,12 +4,12 @@ import { NewsItem } from "@/type/newsData";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const params = request.nextUrl?.searchParams;
-  const query = params.get("query");
-  
+  const query = request.url.split("query=")[1];
+  const decodedQuery = decodeURIComponent(query);
+
   try {
     const { data } = await serverInstance.get(`news.json?query=${encodeURI("스포츠")}&display=100`);
-    const filteredItem = data.items.filter((item: NewsItem) => query === item.title);
+    const filteredItem = data.items.filter((item: NewsItem) => decodedQuery === item.title);
     return new Response(JSON.stringify(filteredItem), {
       headers: {
         "Content-Type": "application/json",
